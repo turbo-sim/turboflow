740c0298d8d012c66c6531e3494b64cc0b31ddf6
roagr <roagr@dtu.dk>
2023-11-13 18:42:29 +0100

    fix geomtry_model.py script

--- projects/dev_scripts/geometry_model.py
+++ projects/dev_scripts/geometry_model.py
@@ -17,17 +17,14 @@ CONFIG_FILE = "config_one_stage.yaml"
 case_data = ml.read_configuration_file(CONFIG_FILE)
 
 
-ml.meanline.validate_axial_turbine_geometry(case_data["geometry_new"])
-geom = ml.meanline.calculate_full_geometry(case_data["geometry_new"])
+ml.meanline.validate_turbine_geometry(case_data["geometry"])
+geom = ml.meanline.calculate_full_geometry(case_data["geometry"])
 ml.print_dict(geom)
-geom_info = ml.meanline.check_axial_turbine_geometry(geom)
-
-print()
-for msg in geom_info:
-    print(msg)
+geom_info = ml.meanline.check_turbine_geometry(geom)
 
 
-# print(np.rad2deg(geom["flaring_angle"]))
+print(geom_info)
+
 
 # DONE: improve logic for angle conventions of rotor/stator
 # DONE: improve logic so tip clearance can be zero for stator
