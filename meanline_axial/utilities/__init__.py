6a5dfa719129728b459f39387c5c72c007417220
roagr <roagr@dtu.dk>
2024-01-10 15:02:09 +0100

    Change __init__.py files to load modules manually (better code introspection from IDE)

--- meanline_axial/utilities/__init__.py
+++ meanline_axial/utilities/__init__.py
@@ -1,11 +1,6 @@
-from glob import glob
-from os.path import basename, dirname, isfile
-
-# Get a list of all Python files in the current directory (excluding __init__.py)
-modules = glob(dirname(__file__) + "/*.py")
-__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
-
-# Import all modules specified in __all__
-for module in __all__:
-    exec(f"from .{module} import *")
+from .configuration import *
+from .file_utils import *
+from .graphics import *
+from .numeric import *
+from .optimization import *
 
