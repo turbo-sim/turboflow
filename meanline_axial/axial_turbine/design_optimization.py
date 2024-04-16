8619707f7413fa43793ef55deed5612f760dd8b7
laboan <lasse.b.anderson@ntnu.no>
2024-04-16 09:06:36 +0200

    Adjustment of initial guess wrt choking model

--- meanline_axial/axial_turbine/design_optimization.py
+++ meanline_axial/axial_turbine/design_optimization.py
@@ -15,8 +15,9 @@ def compute_optimal_turbine(config, initial_guess = None):
     # Get initial guess
     x0 = problem.get_initial_guess(initial_guess = initial_guess)
         
-    # Initialize solver object    
-    solver = psv.OptimizationSolver(problem, x0, display = True)
+    # Initialize solver object  
+    max_iter = config["optimization"]["solver_options"]["max_iter"]  
+    solver = psv.OptimizationSolver(problem, x0, max_iter= max_iter, display = True)
 
     sol = solver.solve()
     solver.plot_convergence_history(savefig = False)
@@ -57,6 +58,8 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         mass_flow = self.reference_values["mass_flow_ref"]
         h_is = self.reference_values["h_out_s"]    
         d_is = self.reference_values["d_out_s"]
+        angle_range = self.reference_values["angle_range"]
+        angle_min = self.reference_values["angle_min"]
 
         # Structure design variables to dictionary (Assume set of design variables) # TODO: Make flexible set of design variables 
         design_variables = dict(zip(self.design_variables_keys, x))
@@ -75,9 +78,11 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         self.geometry["aspect_ratio"] = np.array([value for key, value in design_variables.items() if key.startswith('aspect_ratio')])
         self.geometry["pitch_to_chord"] = np.array([value for key, value in design_variables.items() if key.startswith('pitch_to_chord')])
         self.geometry["trailing_edge_to_opening"] = np.array([value for key, value in design_variables.items() if key.startswith('trailing_edge_to_opening')])
-        self.geometry["thickness_to_chord"] = np.array([value for key, value in design_variables.items() if key.startswith('thickness_to_chord')])
+        # self.geometry["thickness_to_chord"] = np.array([value for key, value in design_variables.items() if key.startswith('thickness_to_chord')])
+        self.geometry["metal_angle_le"] = np.array([value*angle_range + angle_min for key, value in design_variables.items() if key.startswith('metal_angle_le')])
+        self.geometry["metal_angle_te"] = np.array([value*angle_range + angle_min for key, value in design_variables.items() if key.startswith('metal_angle_te')])
         self.geometry = geom.calculate_geometry(self.geometry)
-        self.geometry = geom.calculate_full_geometry(self.geometry)
+        # self.geometry = geom.calculate_full_geometry(self.geometry)
 
         # Evaluate turbine model
         self.results = flow.evaluate_axial_turbine(
@@ -92,17 +97,14 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
             self.f = 0 
         elif self.obj_func == 'efficiency_ts':
             self.f = -self.results["overall"]["efficiency_ts"].values[0]/100
+
         cons = np.array((self.results["overall"]["mass_flow_rate"].values-mass_flow)/mass_flow)
-        # keys_variables = ["plane", "cascade","stage", "overall"]
-        # subset_cascades_data = {key: self.cascades_data[key] for key in keys_variables}
-        # for key in self.constraints_eq.keys():
-        #     variable = find_variable(subset_cascades_data, key)
-        #     cons.append((variable-self.constraints_eq[key])/self.constraints_eq[key])
-            
+        areas = np.array(self.geometry["A_out"][0:-1] - self.geometry["A_in"][1:])
+
         residuals = np.array(list(self.results["residuals"].values()))
-        self.c_eq = np.concatenate((residuals, cons))
+        self.c_eq = np.concatenate((residuals, cons, areas))
         self.c_ineq = None
-        
+
         objective_and_constraints = self.merge_objective_and_constraints(self.f, self.c_eq, self.c_ineq)
         
         return objective_and_constraints
@@ -111,6 +113,7 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         # TODO: improve to do checks in case bounds are given 
         if self.bounds == []:
             self.bounds = self.get_default_bounds()
+
         return self.bounds
             
     def get_n_eq(self):
@@ -142,27 +145,30 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         """
 
         number_of_cascades = len(self.geometry["cascade_type"])
+        new_keys = []
         for key in self.design_variables_keys:
-            if key == "hub_to_tip":
-                   self.design_variables_keys.remove("hub_to_tip")
-                   self.design_variables_keys += [f"hub_to_tip_{i+1}" for i in range(2*number_of_cascades)]
+            if key == "omega_spec":
+                new_keys += ["omega_spec"]
+            elif key == "diameter_spec":
+                new_keys += ["diameter_spec"]
+            elif key == "hub_to_tip":
+                new_keys += [f"hub_to_tip_{i+1}" for i in range(2*number_of_cascades)]
             elif key == "aspect_ratio":
-                   self.design_variables_keys.remove("aspect_ratio")
-                   self.design_variables_keys += [f"aspect_ratio_{i+1}" for i in range(number_of_cascades)]
+                new_keys += [f"aspect_ratio_{i+1}" for i in range(number_of_cascades)]
             elif key == "pitch_to_chord":
-                   self.design_variables_keys.remove("pitch_to_chord")
-                   self.design_variables_keys += [f"pitch_to_chord_{i+1}" for i in range(number_of_cascades)]
+                new_keys += [f"pitch_to_chord_{i+1}" for i in range(number_of_cascades)]
             elif key == "trailing_edge_to_opening":
-                   self.design_variables_keys.remove("trailing_edge_to_opening")
-                   self.design_variables_keys += [f"trailing_edge_to_opening_{i+1}" for i in range(number_of_cascades)]
+                new_keys += [f"trailing_edge_to_opening_{i+1}" for i in range(number_of_cascades)]
             elif key == "thickness_to_chord":
-                   self.design_variables_keys.remove("thickness_to_chord")
-                   self.design_variables_keys += [f"thickness_to_chord_{i+1}" for i in range(number_of_cascades)]
+                new_keys += [f"thickness_to_chord_{i+1}" for i in range(number_of_cascades)]
             elif key == "metal_angle_le":
-                   self.design_variables_keys.remove("metal_angle_le")
-                   self.design_variables_keys += [f"metal_angle_le{i+1}" for i in range(number_of_cascades)]
+                new_keys += [f"metal_angle_le_{i+1}" for i in range(number_of_cascades)]
+            elif key == "metal_angle_te":
+                new_keys += [f"metal_angle_te_{i+1}" for i in range(number_of_cascades)]
 
-        return                       
+        self.design_variables_keys = new_keys
+
+        return                      
 
     def get_default_initial_guess(self):
         """
@@ -173,11 +179,11 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         initial_guess = np.array([]) 
         for key in self.design_variables_keys:
             if key == "omega_spec":
-                initial_guess = np.append(initial_guess, 1.0)
+                initial_guess = np.append(initial_guess, 1.2)
             elif key == "diameter_spec":
-                initial_guess = np.append(initial_guess, 1.0)
+                initial_guess = np.append(initial_guess, 1.2)
             elif key.startswith("hub_to_tip"):
-                initial_guess = np.append(initial_guess, 0.8)
+                initial_guess = np.append(initial_guess, 0.6)
             elif key.startswith("aspect_ratio"):
                 initial_guess = np.append(initial_guess, 1.5)
             elif key.startswith("pitch_to_chord"):
@@ -187,7 +193,16 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
             elif key.startswith("thickness_to_chord"):
                 initial_guess = np.append(initial_guess, 0.2)
             elif key.startswith("metal_angle_le"):
-                initial_guess = np.append(initial_guess, 20*np.pi/180)
+                if int(key[-1]) % 2 == 0:
+                    initial_guess = np.append(initial_guess, 0.41) # -15 degrees for rotor
+                else:
+                    initial_guess = np.append(initial_guess, 0.5) # 0 degrees for stator 
+            elif key.startswith("metal_angle_te"):
+                if int(key[-1]) % 2 == 0:
+                    initial_guess = np.append(initial_guess, 0.17) # -60 degrees for rotor
+                else:
+                    initial_guess = np.append(initial_guess, 0.94) # 80 degrees for stator 
+
         initial_guess = dict(zip(self.design_variables_keys, initial_guess))
 
         # Add vector of independent variables to the initial guess
@@ -198,13 +213,21 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
             initial_guess_variables.update(
                 {
                     "w_out" + index: 0.65,
-                    "s_out" + index: 1.01,
+                    "s_out" + index: 0.15,
                     "beta_out" + index: ((1-2*((i+1) % 2 == 0))*60+90)/180, # Trick to get different guess for stator/rotor
                     "v*_in" + index: 0.4,
-                    "w*_throat" + index: 0.9,
-                    "s*_throat" + index: 1.01,
+                    "beta*_throat" + index: ((1-2*((i+1) % 2 == 0))*60+90)/180,
+                    "w*_throat" + index: 0.65,
+                    "s*_throat" + index: 0.15
                 })
             
+        if self.model_options["choking_model"] == "evaluate_cascade_throat":
+            initial_guess_variables = {key : val for key, val in initial_guess_variables.items() if not key.startswith("v*_in")}
+        elif self.model_options["choking_model"] == "evaluate_cascade_critical":
+            initial_guess_variables = {key : val for key, val in initial_guess_variables.items() if not key.startswith("beta*_throat")}
+        elif self.model_options["choking_model"] == "evaluate_cascade_isentropic_throat":
+            initial_guess_variables = {key : val for key, val in initial_guess_variables.items() if not (key.startswith("v*_in") or key.startswith("s*_throat") or key.startswith("beta*_throat"))}
+
         self.keys = initial_guess_variables.keys() # TODO: Move this
         # self.keys used to merge with independent variables in get_optimization_values
         # This implementation only works with default initial guess
@@ -276,13 +299,11 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         # Calculate exit static properties for a isenthalpic expansion
         state_out_h = self.fluid.get_props(cp.HmassP_INPUTS, h0_in, p_out)
         s_isenthalpic = state_out_h["s"]
-
         # Calculate spouting velocity
         v0 = np.sqrt(2 * (h0_in - h_isentropic))
 
         # Define a reference mass flow rate
         mass_flow_rate = design_point["mass_flow_rate"]
-
         # Define reference_values
         self.reference_values = {
             "s_range": s_isenthalpic - s_in,
@@ -296,7 +317,7 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
         }
 
         return
-
+    
     def get_default_bounds(self):
         """
         Gives default bounds to each design variable.
@@ -306,40 +327,48 @@ class CascadesOptimizationProblem(psv.OptimizationProblem):
 
         for key in self.design_variables_keys:
             if key.startswith("w*_"):
-                bounds += [(0.4, 1.5)]
+                bounds += [(0.1, 1.0)]
             elif key.startswith("w_out"):
-                bounds += [(0.4, 1)]
+                bounds += [(0.1, 1.0)]
             elif key.startswith("v*_in_1"):
                 bounds += [(0.01, 0.8)]
             elif key.startswith("v*_in"):
                 bounds += [(0.1, 1)]
             elif key.startswith("v_in"):
-                bounds += [(0.01, 0.5)]
+                bounds += [(0.001, 0.5)]
             elif key.startswith("beta"): 
                 if int(key[-1]) % 2 == 0: 
                     bounds += [(0.06, 0.28)] # Rotor (-40, -80) [degree]
                 else:
                     bounds += [(0.72, 0.94)] # Stator (40, 80) [degree]
             elif key.startswith("s*"):
-                bounds += [(1.0, 1.2)] # TODO: Check with scaling
+                bounds += [(0.0, 0.32)] 
             elif key.startswith("s_"):
-                bounds += [(1.0, 1.2)] # TODO: Check with scaling
+                bounds += [(0.0, 0.32)]
             elif key == "omega_spec":
-                bounds += [(0.1, 10)]
+                bounds += [(0.01, 10)]
             elif key == "diameter_spec":
-                bounds += [(0.1, 10)]
+                bounds += [(0.01, 10)]
             elif key.startswith("hub_to_tip"):
-                bounds += [(0.6, 95)]
+                bounds += [(0.6, 0.9)]
             elif key.startswith("aspect_ratio"):
                 bounds += [(1.0, 2.0)]
             elif key.startswith("pitch_to_chord"):
                 bounds += [(0.75, 1.10)]
             elif key.startswith("trailing_edge_to_opening"):
-                bounds += [(0.1, 10)]
+                bounds += [(0.05, 0.4)]
             elif key.startswith("thickness_to_chord"):
                 bounds += [(0.15, 0.25)]
             elif key.startswith("metal_angle_le"):
-                bounds += [(0, 60*np.pi/180)] # Fix bounds for stator and rotor
+                if int(key[-1]) % 2 == 0: 
+                    bounds += [(0.41, 0.92)] # Rotor (-15, 75) [degree]
+                else:
+                    bounds += [(0.08, 0.58)] # Stator (-75, 15) [degree]
+            elif key.startswith("metal_angle_te"):
+                if int(key[-1]) % 2 == 0: 
+                    bounds += [(0.06, 0.28)] # Rotor (-40, -80) [degree]
+                else:
+                    bounds += [(0.72, 0.94)] # Stator (40, 80) [degree]
 
         return tuple(bounds)
 
