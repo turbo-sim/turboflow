801134d036d68043584780255f72ca33a11e9b2f
roagr <roagr@dtu.dk>
2024-07-22 13:51:33 +0200

    remove version from index.rst and add it to docs/conf.py instead

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "turboflow"
 copyright = "2024, Lasse Borg Anderson and Roberto Agromayor"
 author = "Lasse Borg Anderson and Roberto Agromayor"
-release = "v0.0"
+release = "v0.1.11"
 
 # Define extensions
 extensions = [
