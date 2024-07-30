01ed4912878efb15a4a8431e4d405296e544e77d
laboan <lasse.b.anderson@ntnu.no>
2024-07-30 10:51:03 +0200

    rename choking criterion models

--- turboflow/axial_turbine/__init__.py
+++ turboflow/axial_turbine/__init__.py
@@ -3,5 +3,5 @@ from .design_optimization import *
 from .deviation_model import *
 from .geometry_model import *
 from .flow_model import *
-from .choking_model import *
+from .choking_criterion import *
 from .loss_model import *
